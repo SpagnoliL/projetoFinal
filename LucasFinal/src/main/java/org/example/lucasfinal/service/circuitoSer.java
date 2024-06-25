9cf03bf71558dad61c816cd548d43b6db3e21def
LucasOstwald <lucas-postwald@educar.rs.gov.br>
2024-06-25 20:07:46 -0300

    primeiro commit lucas

--- LucasFinal/src/main/java/org/example/lucasfinal/service/circuitoSer.java
+++ LucasFinal/src/main/java/org/example/lucasfinal/service/circuitoSer.java
@@ -1,7 +1,28 @@
 package org.example.lucasfinal.service;
 
+import org.example.lucasfinal.entity.circuito;
+import org.example.lucasfinal.repository.circuitoRep;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class circuitoSer {
+    @Autowired
+    circuitoRep circuitoRepository;
+
+    public circuito cadastrarCircuito(circuito Circuito) {
+        return circuitoRepository.save(Circuito);
+    }
+    public List<circuito> listaCircuitos(){
+        return circuitoRepository.findAll();
+    }
+    public circuito updateCircuito(circuito Circuito) {
+        return circuitoRepository.save(Circuito);
+    }
+
+    public void deleteCircuito(Long id) {
+        circuitoRepository.deleteById(id);
+    }
 }
--- LucasFinal/src/main/java/org/example/lucasfinal/service/inscricaoSer.java
+++ LucasFinal/src/main/java/org/example/lucasfinal/service/inscricaoSer.java
@@ -1,7 +1,28 @@
 package org.example.lucasfinal.service;
 
+import org.example.lucasfinal.entity.inscricao;
+import org.example.lucasfinal.repository.inscricaoRep;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Service
 public class inscricaoSer {
+    @Autowired
+    inscricaoRep inscricaoRepository;
+
+    public inscricao cadastrarInscricao(inscricao Inscricao) {
+        return inscricaoRepository.save(Inscricao);
+    }
+    public List<inscricao> listaInscricoes(){
+        return inscricaoRepository.findAll();
+    }
+    public inscricao updateInscricao(inscricao Inscricao) {
+        return inscricaoRepository.save(Inscricao);
+    }
+
+    public void deleteInscricao(Long id) {
+        inscricaoRepository.deleteById(id);
+    }
 }
