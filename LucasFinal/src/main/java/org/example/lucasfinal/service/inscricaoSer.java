b54a53756862f357c41552535e9facf2d7843852
SpagnoliL <lucas-spagnoli@educar.rs.gov.br>
2024-06-25 20:59:03 -0300

    criamento dos guri dos if

--- LucasFinal/src/main/java/org/example/lucasfinal/service/inscricaoSer.java
+++ LucasFinal/src/main/java/org/example/lucasfinal/service/inscricaoSer.java
@@ -1,6 +1,8 @@
 package org.example.lucasfinal.service;
 
+import org.example.lucasfinal.entity.circuito;
 import org.example.lucasfinal.entity.inscricao;
+import org.example.lucasfinal.entity.maratonista;
 import org.example.lucasfinal.repository.inscricaoRep;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +14,49 @@ public class inscricaoSer {
     @Autowired
     inscricaoRep inscricaoRepository;
 
-    public inscricao cadastrarInscricao(inscricao Inscricao) {
-        return inscricaoRepository.save(Inscricao);
+    public String cadastrarInscricao(inscricao Inscricao, maratonista Maratonista, circuito Circuito) {
+
+
+
+
+
+
+
+
+
+
+    if (Circuito.getCategoria().equalsIgnoreCase("Pequeno")){
+   if (Maratonista.getIdade()>=18){
+       if (Inscricao.getValorPrecisao() == 1500){
+           return ""+inscricaoRepository.save(Inscricao);
+       } else if (Inscricao.getValorPrecisao() != 1500){
+           return "Preço merrequento";
+       }
+       } else if (Maratonista.getIdade()<18) {
+       if (Inscricao.getValorPrecisao() == 1300){
+           return ""+inscricaoRepository.save(Inscricao);
+       } else if (Inscricao.getValorPrecisao() != 1300){
+           return "Preço merrequento";
+       }
+   }
+    }
+        if (Circuito.getCategoria().equalsIgnoreCase("Médio")){
+            if (Maratonista.getIdade()>=18){
+                if (Inscricao.getValorPrecisao() == 2300){
+                    return ""+inscricaoRepository.save(Inscricao);
+                } else if (Inscricao.getValorPrecisao() != 2300){
+                    return "Preço merrequento";
+                }
+            } else if (Maratonista.getIdade()<18) {
+                if (Inscricao.getValorPrecisao() == 2000){
+                    return ""+inscricaoRepository.save(Inscricao);
+                } else if (Inscricao.getValorPrecisao() != 2000){
+                    return "Preço merrequento";
+                }
+            }
+        }
+
+    return "abubublé";
     }
     public List<inscricao> listaInscricoes(){
         return inscricaoRepository.findAll();
