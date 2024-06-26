9c87f2058f7cb04b8711dd084204feaf9c9b5449
LucasOstwald <lucas-postwald@educar.rs.gov.br>
2024-06-25 21:02:13 -0300

    inclusao cadastro de inscricao

--- LucasFinal/src/main/java/org/example/lucasfinal/service/inscricaoSer.java
+++ LucasFinal/src/main/java/org/example/lucasfinal/service/inscricaoSer.java
@@ -1,6 +1,8 @@
 package org.example.lucasfinal.service;
 
+import org.example.lucasfinal.entity.circuito;
 import org.example.lucasfinal.entity.inscricao;
+import org.example.lucasfinal.entity.maratonista;
 import org.example.lucasfinal.repository.inscricaoRep;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +14,50 @@ public class inscricaoSer {
     @Autowired
     inscricaoRep inscricaoRepository;
 
-    public inscricao cadastrarInscricao(inscricao Inscricao) {
-        return inscricaoRepository.save(Inscricao);
-    }
+    public inscricao cadastrarInscricao(inscricao Inscricao, maratonista Maratonista, circuito Circuito) {
+        if (Circuito.getCategoria().equalsIgnoreCase("Avancado")){
+            if (Maratonista.getIdade()>=18){
+                if (Inscricao.getValorPrecisao() == 2800){
+                    return inscricaoRepository.save(Inscricao);
+                }
+            } throw new NullPointerException("É preciso ser maior de idade para participar!");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public List<inscricao> listaInscricoes(){
         return inscricaoRepository.findAll();
     }
